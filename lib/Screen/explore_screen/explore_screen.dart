2c6fb2545d76caf496d6590992f5312bfaac8fbf
RI TEII <teiiri@outlook.com>
2022-02-20 19:47:27 +0000

    Refactor code of `ExploreScreen`

--- lib/Screen/explore_screen/explore_screen.dart
+++ lib/Screen/explore_screen/explore_screen.dart
@@ -11,53 +11,53 @@ class ExploreScreen extends StatefulWidget {
 }
 
 class _ExploreScreenState extends State<ExploreScreen> {
+  _ExploreScreenState() {
+    moduleOverviewCardList = <OverviewCard>[
+      const OverviewCard(
+        title: "Dice Roller",
+        subtitle: "RI TEII",
+        description: "Dice are small, throwable objects with marked sides "
+            "that can rest in multiple positions. They are used "
+            "for generating random numbers, commonly as part of "
+            "tabletop games.",
+        icon: ColoredBox(
+          color: Colors.indigo,
+          child: Padding(
+            padding: EdgeInsets.all(2.0),
+            child: Icon(
+              Icons.drag_indicator,
+              size: 28.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        overviewImage: AssetImage("assets/explore/dice_cover.jpg"),
+        routeName: diceRollerHomeScreen,
+      ),
+      const OverviewCard(
+        title: "Empty Module",
+        description: "This a empty module",
+        icon: Image(
+          image: AssetImage("assets/explore/xcode_icon.png"),
+          fit: BoxFit.cover,
+        ),
+        overviewImage: AssetImage("assets/explore/emptyApp_cover.jpg"),
+        routeName: 'null',
+      ),
+    ];
+  }
+
+  late final List<OverviewCard> moduleOverviewCardList;
+
   @override
   Widget build(BuildContext context) {
-    const overviewCardPadding = EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 8.0);
-
     return Scaffold(
       appBar: const ProjectAppBar(title: "Explore"),
-      body: ListView(
-        children: const [
-          Padding(
-            padding: overviewCardPadding,
-            child: OverviewCard(
-              title: "Dice Roller",
-              subtitle: "RI TEII",
-              description:
-                  "Dice are small, throwable objects with marked sides "
-                  "that can rest in multiple positions. They are used "
-                  "for generating random numbers, commonly as part of "
-                  "tabletop games.",
-              icon: ColoredBox(
-                color: Colors.indigo,
-                child: Padding(
-                  padding: EdgeInsets.all(2.0),
-                  child: Icon(
-                    Icons.drag_indicator,
-                    size: 28.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              overviewImage: AssetImage("assets/explore/dice_cover.jpg"),
-              routeName: diceRollerHomeScreen,
-            ),
-          ),
-          Padding(
-            padding: overviewCardPadding,
-            child: OverviewCard(
-              title: "Empty Module",
-              description: "This a empty module",
-              icon: Image(
-                image: AssetImage("assets/explore/xcode_icon.png"),
-                fit: BoxFit.cover,
-              ),
-              overviewImage: AssetImage("assets/explore/emptyApp_cover.jpg"),
-              routeName: 'null',
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: moduleOverviewCardList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return moduleOverviewCardList[index];
+        },
       ),
     );
   }
--- lib/Widget/overview_card.dart
+++ lib/Widget/overview_card.dart
@@ -27,122 +27,125 @@ class _OverviewCardState extends State<OverviewCard> {
   Widget build(BuildContext context) {
     const cardBorderRadius = Radius.circular(32.0);
 
-    return Card(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(cardBorderRadius),
-      ),
-      shadowColor: Colors.transparent,
-      color: const Color.fromRGBO(197, 202, 208, 0.9),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: cardBorderRadius,
-              topRight: cardBorderRadius,
-            ),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Image(
-                image: widget.overviewImage,
-                fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 8.0),
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(cardBorderRadius),
+        ),
+        shadowColor: Colors.transparent,
+        color: const Color.fromRGBO(197, 202, 208, 0.9),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: cardBorderRadius,
+                topRight: cardBorderRadius,
+              ),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Image(
+                  image: widget.overviewImage,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 16.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 12.0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.title,
+                            style: const TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            widget.description,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(widget.routeName);
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "Open",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                    child: SizedBox(
+                      width: 32.0,
+                      height: 32.0,
+                      child: widget.icon,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          widget.title,
-                          style: const TextStyle(
-                            fontSize: 24.0,
+                        const Text(
+                          "Powered by",
+                          style: TextStyle(
+                            fontSize: 12.0,
                             fontWeight: FontWeight.bold,
+                            color: Colors.grey,
                           ),
                         ),
                         Text(
-                          widget.description,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
+                          widget.subtitle,
                           style: const TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w500,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
                         ),
                       ],
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(widget.routeName);
-                  },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                      ),
-                    ),
-                  ),
-                  child: const Text(
-                    "Open",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 16.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-                  child: SizedBox(
-                    width: 32.0,
-                    height: 32.0,
-                    child: widget.icon,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 4.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Powered by",
-                        style: TextStyle(
-                          fontSize: 12.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        widget.subtitle,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
