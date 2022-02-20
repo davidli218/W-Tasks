4e8019fd604cee43001eb8a84d46d728f982ae90
RI TEII <teiiri@outlook.com>
2022-02-20 13:42:26 +0000

    Refactor `OverviewCard` in `ExploreScreen`

--- lib/Screen/explore_screen/explore_screen.dart
+++ lib/Screen/explore_screen/explore_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wtasks/Screen/explore_screen/widgets/overview_card.dart';
 import 'package:wtasks/app_route.dart';
 
 class ExploreScreen extends StatefulWidget {
@@ -16,63 +17,20 @@ class _ExploreScreenState extends State<ExploreScreen> {
         title: const Text("ExplorePage"),
       ),
       body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Card(
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-              ),
-              elevation: 5,
-              child: Column(
-                children: [
-                  const ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
-                    ),
-                    child: AspectRatio(
-                      aspectRatio: 16 / 9,
-                      child: Image(
-                        image: AssetImage("assets/explore/dice_cover.jpg"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  const ListTile(
-                    leading: CircleAvatar(
-                      child: Icon(Icons.drag_indicator),
-                    ),
-                    title: Text("Dice Roller"),
-                    subtitle: Text("TEII RI"),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                    child: const Text(
-                      "Dice are small, throwable objects with marked sides "
-                      "that can rest in multiple positions. They are used "
-                      "for generating random numbers, commonly as part of "
-                      "tabletop games.",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  ButtonBar(
-                    children: [
-                      TextButton(
-                        child: const Text('DETAIL'),
-                        onPressed: () {},
-                      ),
-                      TextButton(
-                        child: const Text('OPEN'),
-                        onPressed: () {
-                          Navigator.of(context).pushNamed(diceRollerHomeScreen);
-                        },
-                      ),
-                    ],
-                  )
-                ],
-              ),
+        children: const [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: OverviewCard(
+              moduleTitle: "Dice Roller",
+              moduleSubtitle: "RI TEII",
+              moduleDescription:
+                  "Dice are small, throwable objects with marked sides "
+                  "that can rest in multiple positions. They are used "
+                  "for generating random numbers, commonly as part of "
+                  "tabletop games.",
+              moduleIcon: Icons.drag_indicator,
+              moduleOverviewImage: AssetImage("assets/explore/dice_cover.jpg"),
+              moduleRouteName: diceRollerHomeScreen,
             ),
           ),
         ],
---
+++ lib/Screen/explore_screen/widgets/overview_card.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class OverviewCard extends StatefulWidget {
+  const OverviewCard(
+      {Key? key,
+      required this.moduleTitle,
+      this.moduleSubtitle = '',
+      required this.moduleDescription,
+      required this.moduleIcon,
+      required this.moduleOverviewImage,
+      required this.moduleRouteName})
+      : super(key: key);
+
+  final String moduleTitle;
+  final String moduleSubtitle;
+  final String moduleDescription;
+  final IconData moduleIcon;
+  final ImageProvider moduleOverviewImage;
+  final String moduleRouteName;
+
+  @override
+  _OverviewCardState createState() => _OverviewCardState();
+}
+
+class _OverviewCardState extends State<OverviewCard> {
+  @override
+  Widget build(BuildContext context) {
+    const cardBorderRadius = Radius.circular(32.0);
+
+    return Card(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(cardBorderRadius),
+      ),
+      shadowColor: Colors.transparent,
+      color: const Color.fromRGBO(197, 202, 208, 0.9),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: cardBorderRadius,
+              topRight: cardBorderRadius,
+            ),
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Image(
+                image: widget.moduleOverviewImage,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.moduleTitle,
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          widget.moduleDescription,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(widget.moduleRouteName);
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    "Open",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24.0, 16.0, 24.0, 16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                  child: ColoredBox(
+                    color: Colors.indigo,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(widget.moduleIcon, color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Powered by",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        widget.moduleSubtitle,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
