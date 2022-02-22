f789ae72dbecba811c3f0658d983a175d8a2cb64
RI TEII <teiiri@outlook.com>
2022-02-22 19:30:49 +0000

    Add an animation when opening the `TaskTile` widget

--- lib/Widget/task_tile.dart
+++ lib/Widget/task_tile.dart
@@ -1,6 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:wtasks/Data/event.dart';
-import 'package:wtasks/app_route.dart';
+import 'package:wtasks/Screen/view_screen/event_details_screen.dart';
 
 class TaskTile extends StatefulWidget {
   const TaskTile({Key? key, required this.event}) : super(key: key);
@@ -22,74 +23,76 @@ class _TaskTileState extends State<TaskTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).pushNamed(eventDetailsScreen);
-        },
-        borderRadius: BorderRadius.circular(8.0),
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 2.0, top: 2.0),
-          child: ListTile(
-            contentPadding: const EdgeInsets.only(left: 2),
-            title: Text(
-              widget.event.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                decoration: widget.event.isFinished
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 4.0),
-              child: Text(
-                widget.event.place.isEmpty
-                    ? _formatTime(widget.event.deadLine)
-                    : widget.event.place,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      child: OpenContainer(
+        transitionType: ContainerTransitionType.fade,
+        useRootNavigator: true,
+        openBuilder: (context, openContainer) => const EventDetailsScreen(),
+        closedShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        closedElevation: 1.0,
+        closedBuilder: (context, openContainer) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 2.0, top: 2.0),
+            child: ListTile(
+              contentPadding: const EdgeInsets.only(left: 2),
+              title: Text(
+                widget.event.title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
                 style: TextStyle(
                   decoration: widget.event.isFinished
                       ? TextDecoration.lineThrough
                       : TextDecoration.none,
                 ),
               ),
-            ),
-            leading: Checkbox(
-              value: widget.event.isFinished,
-              onChanged: (bool? value) {
-                setState(() {
-                  widget.event.isFinished = value!;
-                });
-              },
-            ),
-            trailing: GestureDetector(
-              onTap: () {
-                setState(() {
-                  if (widget.event.isImportant) {
-                    widget.event.isImportant = false;
-                  } else {
-                    widget.event.isImportant = true;
-                  }
-                });
-              },
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                color: Colors.transparent,
-                child: Icon(
-                  widget.event.isImportant
-                      ? Icons.bookmark
-                      : Icons.bookmark_border,
-                  color: widget.event.isImportant ? Colors.deepOrange : null,
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 4.0),
+                child: Text(
+                  widget.event.place.isEmpty
+                      ? _formatTime(widget.event.deadLine)
+                      : widget.event.place,
+                  style: TextStyle(
+                    decoration: widget.event.isFinished
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                  ),
+                ),
+              ),
+              leading: Checkbox(
+                value: widget.event.isFinished,
+                onChanged: (bool? value) {
+                  setState(() {
+                    widget.event.isFinished = value!;
+                  });
+                },
+              ),
+              trailing: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    if (widget.event.isImportant) {
+                      widget.event.isImportant = false;
+                    } else {
+                      widget.event.isImportant = true;
+                    }
+                  });
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  color: Colors.transparent,
+                  child: Icon(
+                    widget.event.isImportant
+                        ? Icons.bookmark
+                        : Icons.bookmark_border,
+                    color: widget.event.isImportant ? Colors.deepOrange : null,
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
