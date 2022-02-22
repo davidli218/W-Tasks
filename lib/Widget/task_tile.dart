0090fc2dcd936b73052630f9fcfc5697572a7550
RI TEII <teiiri@outlook.com>
2022-02-22 19:42:00 +0000

    Add InkWell animation when pressing the `TaskTile` widget

--- lib/Widget/task_tile.dart
+++ lib/Widget/task_tile.dart
@@ -28,65 +28,70 @@ class _TaskTileState extends State<TaskTile> {
       child: OpenContainer(
         transitionType: ContainerTransitionType.fade,
         useRootNavigator: true,
+        tappable: false,
         openBuilder: (context, openContainer) => const EventDetailsScreen(),
         closedShape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8.0),
         ),
         closedElevation: 1.0,
         closedBuilder: (context, openContainer) {
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 2.0, top: 2.0),
-            child: ListTile(
-              contentPadding: const EdgeInsets.only(left: 2),
-              title: Text(
-                widget.event.title,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  decoration: widget.event.isFinished
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 4.0),
-                child: Text(
-                  widget.event.place.isEmpty
-                      ? _formatTime(widget.event.deadLine)
-                      : widget.event.place,
+          return InkWell(
+            onTap: openContainer,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 2.0, top: 2.0),
+              child: ListTile(
+                contentPadding: const EdgeInsets.only(left: 2),
+                title: Text(
+                  widget.event.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                   style: TextStyle(
                     decoration: widget.event.isFinished
                         ? TextDecoration.lineThrough
                         : TextDecoration.none,
                   ),
                 ),
-              ),
-              leading: Checkbox(
-                value: widget.event.isFinished,
-                onChanged: (bool? value) {
-                  setState(() {
-                    widget.event.isFinished = value!;
-                  });
-                },
-              ),
-              trailing: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (widget.event.isImportant) {
-                      widget.event.isImportant = false;
-                    } else {
-                      widget.event.isImportant = true;
-                    }
-                  });
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(16.0),
-                  color: Colors.transparent,
-                  child: Icon(
-                    widget.event.isImportant
-                        ? Icons.bookmark
-                        : Icons.bookmark_border,
-                    color: widget.event.isImportant ? Colors.deepOrange : null,
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Text(
+                    widget.event.place.isEmpty
+                        ? _formatTime(widget.event.deadLine)
+                        : widget.event.place,
+                    style: TextStyle(
+                      decoration: widget.event.isFinished
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                ),
+                leading: Checkbox(
+                  value: widget.event.isFinished,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      widget.event.isFinished = value!;
+                    });
+                  },
+                ),
+                trailing: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (widget.event.isImportant) {
+                        widget.event.isImportant = false;
+                      } else {
+                        widget.event.isImportant = true;
+                      }
+                    });
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    color: Colors.transparent,
+                    child: Icon(
+                      widget.event.isImportant
+                          ? Icons.bookmark
+                          : Icons.bookmark_border,
+                      color:
+                          widget.event.isImportant ? Colors.deepOrange : null,
+                    ),
                   ),
                 ),
               ),
