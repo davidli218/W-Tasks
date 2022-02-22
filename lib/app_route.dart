35247d8792a9a7afcdd59bf446aa8885f98eb1bb
RI TEII <teiiri@outlook.com>
2022-02-22 20:17:11 +0000

    Clean unused routes

--- lib/app_route.dart
+++ lib/app_route.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:wtasks/Screen/app_home.dart';
-import 'package:wtasks/Screen/explore_screen/dice_roller/dice_roller_home.dart';
-import 'package:wtasks/Screen/view_screen/event_details_screen.dart';
 
 const String homeScreen = HomeScreen.routeName;
-const String diceRollerHomeScreen = DiceRollerHomeScreen.routeName;
-const String eventDetailsScreen = EventDetailsScreen.routeName;
 
 final routes = {
   homeScreen: (context) => const HomeScreen(),
-  diceRollerHomeScreen: (context) => const DiceRollerHomeScreen(),
-  eventDetailsScreen: (context) => const EventDetailsScreen(),
 };
 
 class UnknownPage extends StatelessWidget {
